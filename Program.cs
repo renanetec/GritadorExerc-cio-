79cfb59bc87ca2b21e8dee98a8deede81d6b2954
renanetec <renanmenezescarlos21@gmail.com>
2021-03-19 20:19:19 -0300

    Altera tudo e adiciona um comando

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GritadorExerc_cio_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+           string boasvindas = "Olá Professor".ToUpper();
+           string mensagem = "Digite nos parenteses em letra minuscula";
+           string explicação = $"{boasvindas}\n{mensagem}";
+           //escreva a baixo 
+           string escrevaaqui = "";
+  
+
+           Console.WriteLine(explicação);
+
+
+
+
+        }
+    }
+}
